eeb2f063d12fedb240aa1a6f550fe174f7b1e82d
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-02-15 15:25:42 -0300

    Integração com HTML

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def principal():
-    return "Hello World"
+    return render_template("index.html")
+
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
