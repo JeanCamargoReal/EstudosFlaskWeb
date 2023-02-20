a1dedf473cb3c7583f3e0813e0710fe70e2c81da
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-02-20 15:48:00 -0300

    Capturando valores de variável

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def principal():
-    return render_template("index.html")
+    fruta1 = "Morango"
+    fruta2 = "Uva"
+    fruta3 = "Maçã"
+    fruta4 = "Laranja"
+    return render_template("index.html",
+                           fruta1=fruta1,
+                           fruta2=fruta2,
+                           fruta3=fruta3,
+                           fruta4=fruta4)
 
 
 @app.route("/sobre")
