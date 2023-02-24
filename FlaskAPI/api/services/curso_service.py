29e76e72659b917a267b8f746b35145fb4dd9fc4
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-02-24 14:53:00 -0300

    Cria Entity e Service de Curso

---
+++ FlaskAPI/api/services/curso_service.py
@@ -0,0 +1,14 @@
+from api import db
+
+from ..models import curso_model
+
+
+def cadastrar_curso(curso):
+    curso_bd = curso_model.Curso(
+        nome=curso.nome,
+        descricao=curso.descricao,
+        data_publicacao=curso.data_publicacao,
+    )
+
+    db.session.add(curso_bd)
+    db.session.commit()
